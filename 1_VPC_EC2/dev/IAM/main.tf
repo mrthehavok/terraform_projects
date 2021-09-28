57ff1cd2a0e2215781ca037efcf24821b54b97e3
mrthehavok <havok16121989@gmail.com>
2021-09-28 16:19:11 +0300

    IAM is completed

--- 1_VPC_EC2/dev/IAM/main.tf
+++ 1_VPC_EC2/dev/IAM/main.tf
@@ -10,6 +10,15 @@ terraform {
   }
 }
 
+data "terraform_remote_state" "global" {
+  backend = "s3"
+  config = {
+    bucket = "mrthehavok.test.cli"
+    key    = "VPC_EC2/globalvars/terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
+
 locals {
   company_name  = data.terraform_remote_state.global.outputs.company_name
   region_name   = data.terraform_remote_state.global.outputs.region_name
@@ -20,6 +29,7 @@ locals {
 
 #------------------------------------------------------------------------------------------------
 
+/*
 resource "aws_iam_group" "developers" {
   name = "developers"
   path = "/users/"
@@ -28,4 +38,114 @@ resource "aws_iam_group" "developers" {
 resource "aws_iam_group" "administrators" {
   name = "administrators"
   path = "/users/"
+}
+
+*/
+
+module "iam_user_super" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-user"
+  version = "~> 4.3"
+
+  name          = var.root_user_name
+  force_destroy = true
+  pgp_key = "keybase:test"
+
+#  pgp_key = "keybase:${var.root_user_name}"
+
+  password_reset_required = false
+}
+
+module "iam_user_db" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-user"
+  version = "~> 4.3"
+
+  name          = var.db_admin_user_name
+  force_destroy = true
+  pgp_key = "keybase:test"
+
+#  pgp_key = "keybase:${var.db_admin_user_name}"
+
+  password_reset_required = false
+}
+
+module "iam_user_dev1" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-user"
+  version = "~> 4.3"
+
+  name          = var.db_dev_user_name1
+  force_destroy = true
+  pgp_key = "keybase:test"
+
+#  pgp_key = "keybase:${var.db_dev_user_name1}"
+
+  password_reset_required = false
+}
+
+module "iam_user_dev2" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-user"
+  version = "~> 4.3"
+
+  name          = var.db_dev_user_name2
+  force_destroy = true
+  pgp_key = "keybase:test"
+
+#  pgp_key = "keybase:${var.db_dev_user_name2}"
+
+  password_reset_required = false
+}
+
+#------------------------------------------------------------------------------------------------
+
+
+module "iam_group_superadmin" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-group-with-policies"
+  version = "~> 4.3"
+
+  name = var.root_group_name
+
+  group_users = [
+    var.root_user_name 
+  ]
+
+  attach_iam_self_management_policy = true
+
+  custom_group_policy_arns = [
+    "arn:aws:iam::aws:policy/AdministratorAccess",
+  ]
+}
+
+
+module "iam_group_db_admin" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-group-with-policies"
+  version = "~> 4.3"
+
+  name = var.db_admin_group_name
+
+  group_users = [
+    var.db_admin_user_name 
+  ]
+
+  attach_iam_self_management_policy = true
+
+  custom_group_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonRDSFullAccess",
+  ]
+}
+
+module "iam_group_db_dev" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-group-with-policies"
+  version = "~> 4.3"
+
+  name = var.db_dev_group_name
+
+  group_users = [
+    var.db_dev_user_name1,
+    var.db_dev_user_name2
+  ]
+
+  attach_iam_self_management_policy = true
+
+  custom_group_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonRDSReadOnlyAccess",
+  ]
 }
---
+++ 1_VPC_EC2/dev/IAM/variables.tf
@@ -0,0 +1,28 @@
+variable "root_user_name" {
+  default = "superuser"
+}
+
+variable "db_admin_user_name" {
+  default = "db_admin"
+}
+
+variable "db_dev_user_name1" {
+  default = "db_developer1"
+}
+
+variable "db_dev_user_name2" {
+  default = "db_developer2"
+}
+
+
+variable "root_group_name" {
+  default = "superadmins"
+}
+
+variable "db_admin_group_name" {
+  default = "dba"
+}
+
+variable "db_dev_group_name" {
+  default = "db_developers"
+}
