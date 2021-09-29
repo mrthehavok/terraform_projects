99b5b98ef8833a419147be95e8a6fb57c49a2c6b
mrthehavok <havok16121989@gmail.com>
2021-09-29 18:49:56 +0300

    root.tf

---
+++ 1_VPC_EC2/dev/root.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region    =   var.region_name
+}
+
+
+
+
+module "global_vars" {
+  source = "./modules/1_globalvars"
+}
+
+module "IAM" {
+  source = "./modules/2_IAM"
+}
+
+module "SSM" {
+  source = "./modules/3_SSM"
+}
+
+
+module "network" {
+  source = "./modules/4_network"
+}
+
+module "blue_green" {
+  source = "./modules/5_VPC/public"
+}
+
+module "RDS" {
+  source = "./modules/6_RDS"
+}
